7a36ff5c40e7a1465b3c1f6fea2fe24f0625659b
Peter <mrpeterfulop@gmail.com>
2020-07-27 22:59:15 +0200

    DOM alapok

--- JS/scope.js
+++ JS/scope.js
@@ -1,10 +1,45 @@
-let fruits =['Alma','Körte','Cseresznye','Dinnye','Szilva','Meggy','Barack','Szőlő','Banán','Ananász','Avokádó','Kivi','Málna','Áfonya','Szeder']
+function runTest1() {
+    let fruits = ['Alma', 'Körte', 'Cseresznye', 'Dinnye', 'Szilva', 'Meggy', 'Barack', 'Szőlő', 'Banán', 'Ananász', 'Avokádó', 'Kivi', 'Málna', 'Áfonya', 'Szeder']
 
 
+    for (let i = 0; i < fruits.length; i++) {
 
-    function runTest1() {
-        for (let i = 0; i > fruits.length; i++) {
-            console.log(i);
-            }
+        if (fruits[i].charAt(0) == "B") {
+            console.log(fruits[i]);
+        }
 
     }
+
+
+}
+/*
+function massModify(selector, attribute, value) {
+    let nodeList = document.querySelectorAll(selector);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i][attribute] = value;
+    }
+}
+*/
+/*
+window.onload = function () {
+
+    let orderForm = document.querySelector("#orderForm");
+    orderForm.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+
+        let inputs = this.querySelectorAll("input");
+        let values = {};
+
+        for (let i = 0; i < inputs.length; i++) {
+            values[inputs[i].name] = inputs[i].value;    
+        }
+
+        console.log( values );
+
+    });
+
+  
+
+}*/
+
+
